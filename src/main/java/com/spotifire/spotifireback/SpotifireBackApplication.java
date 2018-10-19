d7267329bfbe1cab4d8cfe87a54ebb4668d5b209
GRUPOGMV\jmpt <jmpt@gmv.es>
2018-10-20 00:28:59 +0200

    Inicialización de proyecto spring boot

---
+++ src/main/java/com/spotifire/spotifireback/SpotifireBackApplication.java
@@ -0,0 +1,12 @@
+package com.spotifire.spotifireback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifireBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifireBackApplication.class, args);
+	}
+}
