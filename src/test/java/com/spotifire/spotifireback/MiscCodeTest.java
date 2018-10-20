7ae753863113ad6156e86b3760b872f9103a8d4d
Jose <jopetor2@gmail.com>
2018-10-20 22:09:42 +0200

    Second version of the incredible Weather risk value algorithm.

--- src/test/java/com/spotifire/spotifireback/MiscCodeTest.java
+++ src/test/java/com/spotifire/spotifireback/MiscCodeTest.java
@@ -25,7 +25,8 @@ import com.spotifire.web.rest.dto.WeatherDTO;
 
 /**
  *
- * Para probar código suelto, sin referencias al contexto de Spring y sus service, manager y dao
+ * Para probar código suelto, sin referencias al contexto de Spring y sus
+ * service, manager y dao
  *
  * @author aars
  *
@@ -56,16 +57,64 @@ public class MiscCodeTest {
 		float latitude = 37.035229f;
 		float longitude = -6.435476f;
 		long timeStamp = (new Date().getTime()) / 1000;
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("https://api.darksky.net/forecast/f0882429cb4afe2fb72984e427e6789f/").append(latitude).append(",")
 				.append(longitude).append(",").append(timeStamp)
 				.append("?units=si&exclude=flags?exclude=alerts?exclude=minutely?exclude=hourly?exclude=daily");
 		URI uriPetition = new URI(sb.toString());
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<WeatherDTO> response = restTemplate.exchange(uriPetition, HttpMethod.GET, null, WeatherDTO.class);
-		
+		ResponseEntity<WeatherDTO> response = restTemplate.exchange(uriPetition, HttpMethod.GET, null,
+				WeatherDTO.class);
+
 		System.out.println(response.getStatusCode() + ":" + response.getBody());
+
+		int riskFactor = 0;
+
+		double humidity = response.getBody().getCurrently().getHumidity();
+		double precipIntensity = response.getBody().getCurrently().getPrecipIntensity();
+		double precipProbability = response.getBody().getCurrently().getPrecipProbability();
+		double windSpeed = response.getBody().getCurrently().getWindSpeed();
+
+		if (windSpeed > 11) { // 40 km/h
+			riskFactor += 0.2;
+			if (windSpeed > 27) { // 100 km/h
+				riskFactor += 0.2;
+			}
+		}
+
+		if (humidity < 0.5) {
+			riskFactor += 0.2;
+			if (humidity < 0.25) {
+				riskFactor += 0.2;
+			}
+		}
+
+		if (precipProbability > 0.2) {
+			if (precipProbability > 0.4) {
+				riskFactor -= 0.1;
+				if (precipProbability > 0.8) {
+					riskFactor -= 0.1;
+				}
+			}
+		} else {
+			riskFactor += 0.2;
+		}
+
+		if (precipIntensity > 1) {
+			riskFactor -= 0.1;
+			if (precipIntensity > 3) {
+				riskFactor -= 0.1;
+				if (precipIntensity > 5) {
+					riskFactor -= 0.1;
+					if (precipIntensity > 10) {
+						riskFactor = 0;
+					}
+				}
+			}
+		} else {
+			riskFactor += 0.1;
+		}
 
 		System.out.println("Test End");
 	}
